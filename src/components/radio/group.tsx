040d26cd33dadba14d5583ba5914263b5f4cd31c
Nguyen Quoc Huy <nerorain1102@gmail.com>
2020-08-16 11:24:24 +0700

    Update antd lib v4.5.4

--- src/components/radio/group.tsx
+++ src/components/radio/group.tsx
@@ -1,6 +1,10 @@
 import { Radio as AntRadio } from 'antd'
-import { RadioGroupProps } from 'antd/es/radio'
+import { RadioGroupProps, RadioGroupOptionType, RadioGroupButtonStyle } from 'antd/es/radio'
 
-export { RadioGroupProps as MocaRadioGroupProps }
+export {
+  RadioGroupProps as MocaRadioGroupProps,
+  RadioGroupOptionType as MocaRadioGroupOptionType,
+  RadioGroupButtonStyle as MocaRadioGroupButtonStyle,
+}
 export const MocaRadioGroup = AntRadio.Group
 export default MocaRadioGroup
